b6759717bf00c5b8cbcb08feb78ab82952bcc4fd
JulianPaz <julianpaz@udenar.edu.co>
2023-08-24 21:01:26 -0500

    Eliminacion de archivos

--- database/migrations/2023_08_14_023217_pais.php
+++ database/migrations/2023_08_14_023217_pais.php
@@ -13,20 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-<<<<<<<< HEAD:database/migrations/2023_08_14_023217_pais.php
         Schema::create('paises', function (Blueprint $table) {
-            $table->id(); 
-            $table->string('nombre', 100);
-            $table->boolean('estaactivo')->default(true); 
-            $table->timestamps();
-========
-        Schema::create('sedes', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre',100);
-            $table->int('idciudad');
-            $table->foreign('idciudad')->references('id')->on('ciudad');
+            $table->string('nombre', 100);
+            $table->boolean('estaactivo')->default(true);
+            $table->timestamps();
 
->>>>>>>> origin/rm_pedrotoro:database/migrations/2023_08_14_055322_create_sedes_table.php
         });
     }
 
@@ -37,10 +29,7 @@ return new class extends Migration
      */
     public function down()
     {
-<<<<<<<< HEAD:database/migrations/2023_08_14_023217_pais.php
+
         Schema::dropIfExists('paises');
-========
-        Schema::dropIfExists('sedes');
->>>>>>>> origin/rm_pedrotoro:database/migrations/2023_08_14_055322_create_sedes_table.php
     }
 };
